10ec647f903ac0fcddfb4a15ae87433e1ded7402
Ruslan Sagirov <justrus93@mail.ru>
2023-06-17 21:30:24 +0300

    thirdD day

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+let phoneInput = document.querySelector('.input-form');
+let btnNext = Array.from(document.getElementsByClassName('next-btn'));
+let backBtn = Array.from(document.getElementsByClassName('back-btn'));
+let tabContent = Array.from(document.getElementsByClassName('none'));
+let tab1 = document.getElementsByClassName('tabs1')
+let main = document.querySelector('main');
+let nickName = document.getElementById('field-nickname');
+let namem = document.getElementById('field-name');
+let surname = document.getElementById('field-surname');
+
+
 function validation(form){
 
     function removeError(input){
@@ -18,7 +29,6 @@ function validation(form){
         
         parent.classList.add('error');
         parent.append(errorLable);
-        console.log(parent);
     }
 
 
@@ -31,7 +41,6 @@ function validation(form){
 
         if(input.dataset.required == 'true'){
             if(input.value == ''){
-                console.log('error');
                 createError(input, 'Поле не заполнено');
                 result = false;
             }
@@ -40,15 +49,24 @@ function validation(form){
     return result;
 }
 
+
+
 document.getElementById('add-form').addEventListener('submit', function(event){
     event.preventDefault();
     if(validation(this) === true){
-        alert('all okay');
+        tab1[0].classList.add('active');
+        main.classList.add('none');
         console.log('все норм'); // сюда написать куда отправлять 
     };
 });
 
-let phoneInput = document.querySelector('.input-form');
+document.getElementById('button-send').addEventListener('click', function(event){
+    event.preventDefault();
+    if(validation(this) === true){
+        alert('отправлена')
+        console.log('все норм'); // сюда написать куда отправлять 
+    };
+});
 
 phoneInput.addEventListener('input', () =>{
     let phoneNumber = phoneInput.value;
@@ -96,3 +114,72 @@ phoneInput.addEventListener('keydown', (event) => {
       phoneInput.value = formattedPhoneNumber;
     }
   });
+
+btnNext.forEach((button, index) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        showTabs(index + 1);
+    });
+});
+
+backBtn.forEach((button, index) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(index === 0 ){
+            main.classList.remove('none');
+        }
+        showTabs(index - 1);
+    })
+});
+
+function showTabs(index){
+    tabContent.forEach((tab) => {
+        tab.classList.remove('active');
+    });
+    tabContent[index].classList.add('active');
+}
+
+
+function inputName(name, length){
+    name.addEventListener('input', (e) => {
+        const maxLength = length;
+        const input = e.target;
+        const value = input.value;
+        const filteredValue = value.replace(/[^a-zA-Z0-9]/g, '').substring(0, maxLength);
+        input.value = filteredValue;
+    });
+};
+inputName(nickName, 30);
+inputName(namem, 50);
+inputName(surname, 50);
+
+
+// let addInput = document.getElementById('button-add');
+// let inputContainer = document.getElementsByClassName('input-box2');
+
+// addInput.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const inputBox = document.createElement('div');
+//     inputBox.classList.add('input-box2');
+    
+//     const inputFields = document.querySelectorAll('.tabs2__input');
+//     const lastInput = inputFields[inputFields.length - 1];
+//     const lastId = parseInt(lastInput.id.split('-')[1]);
+//     const newId = `field-advantages-${lastId + 1}`;
+
+//     const newInput = document.createElement('input');
+//     newInput.type = 'text';
+//     newInput.classList.add('tabs2__input');
+//     newInput.id = newId;
+//     newInput.required = true;
+
+//     const removeBtn = document.createElement('button');
+//     removeBtn.classList.add('.bscet');
+//     removeBtn.textContent = 'del';
+//     removeBtn.addEventListener('click', () => {
+//         inputContainer.removeChild(inputBox); // Удаление строки с полем ввода
+//       });
+//     inputBox.appendChild(newInput);
+//     inputBox.appendChild(removeBtn);
+//     console.log('hey');
+// });
